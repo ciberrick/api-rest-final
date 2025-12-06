df3ff91d9f0327ddf343f2aad519bf92c78de093
§DarkRichi§ <ricardoalcarazfca@gmail.com>
2025-12-06 15:13:13 -0600

    Ajusta PORT para despliegue en Render

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const db = require('./queries');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
